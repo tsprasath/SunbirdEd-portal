c5d42c1b972121525f9a4ddcd17f79145cf264df
Prasath Sivasubramaniyan <prasath.sivasubramaniyan@trigyn.com>
2023-12-26 15:55:39 +0530

    Added dockefile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,53 @@
-# Dockerfile for the player setup
+# Use the official Node.js image with the specified version
+FROM node:14.19.1-alpine AS builder
+
+# Set an environment variable to increase memory limit for Node.js
+ENV NODE_OPTIONS="--max_old_space_size=8096"
+
+# Install git and python
+RUN apk --no-cache add git make python3 autoconf g++ nasm bzip2
+
+# Set python alias for compatibility
+RUN ln -s /usr/bin/python3 /usr/bin/python
+
+# Install Angular CLI globally
+RUN npm install --global @angular/cli@11
+# Copy the entire project to the working directory
+COPY . .
+# Building Client
+RUN cd src/app && \
+    mkdir -p app_dist && \
+    rm -rf dist-cdn && \
+    cd client && \
+    yarn install --no-progress --production=true --ignore-engines && \
+    npm run build && \
+    cd ../ && \
+    mv app_dist/dist/index.html app_dist/dist/index.ejs
+
+# Building Server
+WORKDIR src/app
+COPY src/app/helpers app_dist/helpers
+COPY src/app/proxy app_dist/proxy
+COPY src/app/resourcebundles app_dist/resourcebundles
+COPY src/app/package.json app_dist/package.json
+COPY src/app/framework.config.js app_dist/framework.config.js
+COPY src/app/sunbird-plugins app_dist/sunbird-plugins
+COPY src/app/routes app_dist/routes
+COPY src/app/constants app_dist/constants
+COPY src/app/controllers app_dist/controllers
+COPY src/app/server.js app_dist/server.js
+
+RUN cd app_dist && \
+    yarn install --no-progress --production=true --ignore-engines && \
+    node helpers/resourceBundles/build.js -task="phraseAppPull" && \
+    mkdir -p node_modules/client-cloud-services/dist
+
+
 FROM node:14.19.0-slim
 RUN useradd -u 1001 -md /home/sunbird sunbird
 WORKDIR /home/sunbird
-COPY --chown=sunbird . /home/sunbird/app_dist/
+COPY --chown=sunbird  --from=builder src/app/app_dist /home/sunbird/app_dist/
+COPY --chown=sunbird  --from=builder bundle.js /home/sunbird/app_dist/node_modules/client-cloud-services/dist/bundle.js
 USER sunbird
 WORKDIR /home/sunbird/app_dist
 EXPOSE 3000
--- build.sh
+++ build.sh
@@ -22,76 +22,76 @@ fi
 commit_hash=$(git rev-parse --short HEAD)
 #nvm install $NODE_VERSION # same is used in client and server
 
-cd src/app
-mkdir -p app_dist/ # this folder should be created prior server and client build
-rm -rf dist-cdn # remove cdn dist folder
+# cd src/app
+# mkdir -p app_dist/ # this folder should be created prior server and client build
+# rm -rf dist-cdn # remove cdn dist folder
 
-# function to run client build for docker image
-build_client_docker(){
-    echo "starting client local prod build"
-    npm run build # Angular prod build
-    echo "completed client local prod build"
-    cd ..
-    mv app_dist/dist/index.html app_dist/dist/index.ejs # rename index file
-}
-# function to run client build for cdn
-build_client_cdn(){
-    echo "starting client cdn prod build"
-    npm run build-cdn -- --deployUrl $cdnUrl # prod command
-    export sunbird_portal_cdn_url=$cdnUrl # required for inject-cdn-fallback task
-    npm run inject-cdn-fallback
-    echo "completed client cdn prod build"
-}
-# function to run client build
-build_client(){
-    echo "Building client in background"
-    #nvm use $NODE_VERSION
-    cd client
-    echo "starting client yarn install"
-    yarn install --no-progress --production=true --ignore-engines
-    echo "completed client yarn install"
-    if [ $buildDockerImage == true ]
-    then
-    build_client_docker & # run client local build in background
-    fi
-    if [ $buildCdnAssests == true ]
-    then
-    build_client_cdn & # run client local build in background
-    fi
-    wait # wait for both build to complete
-    echo "completed client post_build"
-}
+# # function to run client build for docker image
+# build_client_docker(){
+#     echo "starting client local prod build"
+#     npm run build # Angular prod build
+#     echo "completed client local prod build"
+#     cd ..
+#     mv app_dist/dist/index.html app_dist/dist/index.ejs # rename index file
+# }
+# # function to run client build for cdn
+# build_client_cdn(){
+#     echo "starting client cdn prod build"
+#     npm run build-cdn -- --deployUrl $cdnUrl # prod command
+#     export sunbird_portal_cdn_url=$cdnUrl # required for inject-cdn-fallback task
+#     npm run inject-cdn-fallback
+#     echo "completed client cdn prod build"
+# }
+# # function to run client build
+# build_client(){
+#     echo "Building client in background"
+#     #nvm use $NODE_VERSION
+#     cd client
+#     echo "starting client yarn install"
+#     yarn install --no-progress --production=true --ignore-engines
+#     echo "completed client yarn install"
+#     if [ $buildDockerImage == true ]
+#     then
+#     build_client_docker & # run client local build in background
+#     fi
+#     if [ $buildCdnAssests == true ]
+#     then
+#     build_client_cdn & # run client local build in background
+#     fi
+#     wait # wait for both build to complete
+#     echo "completed client post_build"
+# }
 
-# function to run server build
-build_server(){
-    echo "Building server in background"
-    echo "copying requied files to app_dist"
-    cp -R libs helpers proxy resourcebundles package.json framework.config.js sunbird-plugins routes constants controllers server.js ./../../Dockerfile app_dist
-    cd app_dist
-    #nvm use $NODE_VERSION
-    echo "starting server yarn install"
-    yarn install --no-progress --production=true --ignore-engines
-    echo "completed server yarn install"
-    node helpers/resourceBundles/build.js -task="phraseAppPull"
-}
+# # function to run server build
+# build_server(){
+#     echo "Building server in background"
+#     echo "copying requied files to app_dist"
+#     cp -R libs helpers proxy resourcebundles package.json framework.config.js sunbird-plugins routes constants controllers server.js ./../../Dockerfile app_dist
+#     cd app_dist
+#     #nvm use $NODE_VERSION
+#     echo "starting server yarn install"
+#     yarn install --no-progress --production=true --ignore-engines
+#     echo "completed server yarn install"
+#     node helpers/resourceBundles/build.js -task="phraseAppPull"
+# }
 
-build_client & # run client build in background
+# build_client & # run client build in background
+# if [ $buildDockerImage == true ]
+# then
+#    build_server & # run client build in background
+# fi
+
+# ## wait for both build to complete
+# wait
+
+# BUILD_ENDTIME=$(date +%s)
+# echo "Client and Server Build complete Took $[$BUILD_ENDTIME - $STARTTIME] seconds to complete."
+
 if [ $buildDockerImage == true ]
-then
-   build_server & # run client build in background
-fi
-
-## wait for both build to complete
-wait
-
-BUILD_ENDTIME=$(date +%s)
-echo "Client and Server Build complete Took $[$BUILD_ENDTIME - $STARTTIME] seconds to complete."
-
-if [ $buildDockerImage == true ]
-then
-cd app_dist
-mkdir -p node_modules/client-cloud-services/dist
-cp /var/lib/jenkins/custombuild/client-cloud-services/bundle.js node_modules/client-cloud-services/dist/
+# then
+# cd app_dist
+# mkdir -p node_modules/client-cloud-services/dist
+# cp /var/lib/jenkins/custombuild/client-cloud-services/bundle.js node_modules/client-cloud-services/dist/
 # you will need to inject the custom client-cloud-service bundle.js to the player build
 # the actual file location will depends on your bundle.js location
 #    1. build the custom client-cloud-services from https://github.com/ocisunbird/client-cloud-services/tree/oci-5.1.0
